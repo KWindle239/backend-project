029f65a2e9174c21f692ad3747b93266574258ad
KWindle239 <kate_windle@hotmail.com>
2024-01-19 13:11:03 +0000

    GET/api/articles?topic - ticket 11 finished

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,8 +1,22 @@
 const { findArticles } = require("../models/articles.model");
+const { checkTopicExists} = require("../check-exists");
 
 const getArticles = (req, res, next) => {
-    findArticles().then((articles) => {
-        res.status(200).send({ articles })
+    const criteria = req.query;
+    topic = criteria.topic;
+
+    const findQuery = findArticles(topic);
+    const queries = [findQuery];
+
+    if(topic){
+        const topicExistenceQuery = checkTopicExists(topic);
+        queries.push(topicExistenceQuery);
+    }
+
+    Promise.all(queries)
+        .then((response) => {
+            const articles = response[0];
+        res.status(200).send({ articles})
     })
     .catch((err) => {
         next(err);
